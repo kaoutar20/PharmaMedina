493ca83369b8e98dbc2d5b2a885b26fd1d440992
dembele4004 <dembeledaouda4004@gmail.com>
2024-02-28 16:17:53 +0100

    La correction du service

--- OrdersServices/Orders/order/models.py
+++ OrdersServices/Orders/order/models.py
@@ -1,32 +1,7 @@
 from django.db import models
 
 # Create your models here.
-#Le modele produit 
-class Product(models.Model):
-    id = models.AutoField(primary_key=True)
-    reference = models.CharField(max_length=255)
-    nom = models.CharField(max_length=255)
-    marque = models.CharField(max_length=255)
-    category = models.CharField(max_length=255)
-    prix = models.BigIntegerField()
-    quantite_stock = models.FloatField()
-    description = models.TextField(max_length=500)
-    indications = models.TextField(max_length=200)
-    mode_emploi = models.TextField(max_length=2000)
-    composition = models.TextField(max_length=2000)
 
-    def __str__(self):
-        return self.nom
-
-#Le modele utilisateur
-class CustomUser(models.Model):
-    id = models.AutoField(primary_key=True)
-    role = models.CharField(max_length=20)
-    groups = models.CharField(max_length=100)
-    email = models.CharField(max_length=200)
-
-
-#Le modele commande 
 class Orders(models.Model):
     STATUS_CHOICES = [
         ('En attente', 'En attente'),
@@ -36,13 +11,23 @@ class Orders(models.Model):
     ]
 
     ORDER_TYPE_CHOICES = [
-        ('En ligne', 'En ligne'),
-        ('En personne', 'En personne'),
+        ('Réapprovisionnement', 'Réapprovisionnement'),
+        ('Urgence', 'Urgence'),
+        ('Régulière Automatique', 'Régulière Automatique'),
     ]
+
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     delivery_date = models.DateField()
     status = models.CharField(max_length=200, choices=STATUS_CHOICES, default='En attente')
     order_type = models.CharField(max_length=200, choices=ORDER_TYPE_CHOICES, default='En ligne')
-    productId = models.ForeignKey(Product, on_delete=models.CASCADE)
-    clientId = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    productId = models.IntegerField()
+    clientId = models.IntegerField()
+
+    def get_product_details(self):
+        response = requests.get(f'http://service-produits/api/products/{self.productId}/')
+        return response.json()
+
+    def get_user_details(self):
+        response = requests.get(f'http://service-utilisateurs/api/users/{self.clientId}/')
+        return response.json()
