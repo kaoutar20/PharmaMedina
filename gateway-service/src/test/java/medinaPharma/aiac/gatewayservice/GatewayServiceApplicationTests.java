6c9187fe5a89115bd9ec2171efbd5d611164758c
BOUCHRA Aicha <102734783+aichabouchra@users.noreply.github.com>
2024-02-29 09:54:22 +0100

    Le micro-service Gateway.

---
+++ gateway-service/src/test/java/medinaPharma/aiac/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package medinaPharma.aiac.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
